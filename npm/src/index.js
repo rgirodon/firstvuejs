8e371507c0f5bfc5a00ad31de8faf8225e292bf6
rgirodon <remy.girodon@gmail.com>
2019-02-19 06:04:06 -0500

    Set up advanced binding

--- npm/src/index.js
+++ npm/src/index.js
@@ -5,6 +5,10 @@ let Vue = require('vue');
 let vueInstance = new Vue({	
     el: '#app',
     data: {
-        message: 'Hello World !'
+        message: 'Hello World !',
+        subtitle: 'Discovering VueJs binding',
+        description: 'This tutorial illustrates the power of <em>binding</em>',
+        provider: 'Design Tech Académie',
+        providerUrl: 'https://www.telecom-st-etienne.fr/formations/design-tech-academie-2/'
     }
 });
